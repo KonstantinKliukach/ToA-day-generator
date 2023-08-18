a1f600001aa07df5f605c63141177f8536950ce5
Константин Клюкач <jfrost@192.168.1.5>
2023-08-18 15:36:58 +0300

    [Feature] Add day panel

---
+++ src/components/Days-Add-Panel.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Button from "../components/Custom-Button.vue";
+import useDaysOfadventureStore from "../stores/daysOfAdventure";
+
+const days = useDaysOfadventureStore();
+
+const handleSubmit = () => {
+  days.addDay();
+};
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" class="card-container">
+    <div class="button-block">
+      <Button type="submit" :disabled="days.loading">Добавить день</Button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.button-block {
+  display: flex;
+  flex-direction: column;
+}
+.card-container {
+  padding: 16px;
+  border-radius: 4px;
+  box-shadow: var(--block-shadow);
+  overflow: hidden;
+}
+</style>
--- src/views/DaysView.vue
+++ src/views/DaysView.vue
@@ -2,6 +2,7 @@
 import useDaysOfadventureStore from "../stores/daysOfAdventure";
 import usedDaysFiltersStore from "../stores/daysFilters";
 import DaysFiltersPanel from "../components/Days-Filters-Panel.vue";
+import DaysAddPanel from "../components/Days-Add-Panel.vue";
 import DayCard from "../components/Day-Card.vue";
 
 const filters = usedDaysFiltersStore();
@@ -10,7 +11,10 @@ days.getDays(filters.getFilters);
 </script>
 <template>
   <main class="layout-container">
-    <aside><DaysFiltersPanel /></aside>
+    <aside class="control-panels">
+      <DaysAddPanel />
+      <DaysFiltersPanel />
+    </aside>
     <main class="grid-container" v-if="!days.loading">
       <DayCard v-for="day in days.daysGetter" :day="day" :key="day._id" />
     </main>
@@ -24,6 +28,11 @@ days.getDays(filters.getFilters);
 </template>
 
 <style scoped>
+.control-panels {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
 .grid-container {
   display: grid;
   gap: 20px;
